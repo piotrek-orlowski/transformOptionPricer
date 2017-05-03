e4a156921c3889ccc3dd25cedf3162a6523adba3
Piotrek Orłowski <piotrek.orlowski@gmail.com>
2017-05-03 10:19:39 +0000

    specified in which envir v.0 has to be evaluated

--- R/gaussLaguerrePricerBS.R
+++ R/gaussLaguerrePricerBS.R
@@ -11,9 +11,9 @@
 #' @return list of arrays of size \code{T x K x S} with relative prices of call options, put options, out of the money options.
 #' @details In extensive tests this pricer yields results comparable to the Fourier-cosine series pricer, with fewer characteristic function evaluations. Care should be taken at very high values of variance factors.
 #' @export
-#' 
+#'
 gaussLaguerrePricer <- function(strikeMat, mkt, alpha=0, N=64, sigma.ref = NULL, N.factors = 3, charFun, preCalc = NULL, ...) {
-  
+
   dimStrikeMat <- dim(strikeMat)
   T <- dimStrikeMat[1]
   K <- dimStrikeMat[2]
@@ -23,44 +23,44 @@ gaussLaguerrePricer <- function(strikeMat, mkt, alpha=0, N=64, sigma.ref = NULL,
   } else {
     S <- dimStrikeMat[3]
   }
-	
+
 	# if CF values are not pre-calculated then calculate them
 	if (is.null(preCalc)) {
-	  
+
 	  require('statmod')
 	  weightAndnodes <- gauss.quad(N,'laguerre',alpha=alpha)
-	  uVec <- weightAndnodes$nodes    
+	  uVec <- weightAndnodes$nodes
 	  # cfVal <- twoFactorJumpODEsSolve(u=cbind(matrix(1i*uVec,ncol=1),matrix(0,N,N.factors)),params.Q,mkt,rtol=rtol,N.factors=N.factors)
 	  cfVal <- charFun(u = cbind(matrix(1i*uVec,ncol=1),matrix(0,N,N.factors)), t.vec = NULL, N.factors = N.factors, mkt = mkt, ...)
-	  
+
 	} else {
-	  
+
 	  weightAndnodes <- preCalc$weightAndnodes
 	  uVec <- weightAndnodes$nodes
 	  cfVal <- preCalc$cfVal
 	}
-	
+
 	F <- exp(mkt$t*(mkt$r-mkt$q))
-	
+
 	if(!is.null(sigma.ref)){
 	  V <- sigma.ref
 	} else {
 	  dots.arg <- as.list(substitute(list(...)))[-1L]
-	  stateVec <- eval(dots.arg[["v.0"]])
+	  stateVec <- eval(dots.arg[["v.0"]], envir = parent.frame())
 	  V <- if(!is.null(dim(stateVec))) rowSums(stateVec) else stateVec
 	  V[which(V > 0.5)] <- 0.5
 	}
-	
+
 	# create B-S c.f. array TxKxSxN dimensions
 	M1 <- matrix(log(F),T,1) %*% matrix(1,1,S)
 	M2 <- matrix(mkt$t,T,1) %*% matrix(V,1,S)
-	
+
 	M3 <- array(rep(as.vector(2*M1-M2),N)*rep(uVec,each = T*S),c(T,S,N))
 	M4 <- array(rep(as.vector(-M2),N)*rep(uVec^2,each = T*S),c(T,S,N))
-	
+
 	cfBS <- aperm(array(exp(0.5 * rep(as.vector(M3*1i+M4),K)),c(T,S,N,K)),c(1,4,2,3))
 
-  
+
 	# create B-S price matrix
 	s <- aperm(array(rep(as.vector(M2),K),c(T,S,K)),c(1,3,2))
 	m <- array(rep(F,S*K),c(T,K,S))
@@ -77,7 +77,7 @@ gaussLaguerrePricer <- function(strikeMat, mkt, alpha=0, N=64, sigma.ref = NULL,
 # 	  cfODE <- cfODE + aperm(array(rep(rep(as.vector(cfVal[,,paste0("b",nn)]),S)*rep(stateVec[,nn],each=N*T),K),c(N,T,S,K)),c(2,4,3,1))
 #   }
 # 	cfODE <- exp(cfODE)
-	
+
 	dCF <-  (cfVal - cfBS) / (1i * aperm(array(rep(uVec^(alpha+1),T*K*S),c(N,T,K,S)),c(2,3,4,1))* (1-1i* aperm(array(rep(uVec,T*K*S),c(N,T,K,S)),c(2,3,4,1))))
 
 	cExp <- -1i*array(rep(as.vector(strikeMat),N),c(T,K,S,N))
@@ -86,23 +86,23 @@ gaussLaguerrePricer <- function(strikeMat, mkt, alpha=0, N=64, sigma.ref = NULL,
 
 	# create u Mat
 	dC <- dCF*cExp
-	
+
 	dC <- dC * aperm(array(rep(weightAndnodes$weights*exp(uVec),T*K*S),c(N,T,K,S)),c(2,3,4,1))
 	call <- rowSums(array(dC[,,,],c(T,K,S,N)),dims=3)
-	
+
 	df1 <- array(rep(exp(-mkt$r*mkt$t),K*S),c(T,K,S))
 	df2 <- array(rep(exp(-mkt$q*mkt$t),K*S),c(T,K,S))
-		
+
 	call <- -exp(strikeMat)*df1/pi * Re(call) + pBS
-	
-	
+
+
 	# now use put-call parity for put prices
 	put <- call + exp(strikeMat)*df1 - 1*df2
-	
+
 	# now get otm
 	otm <- call
 	otm[strikeMat < 0] <- put[strikeMat < 0]
-	
+
 	if("day" %in% names(mkt)){
 		return(list(day = mkt$day, call = call, put = put, otm = otm))
 	}
