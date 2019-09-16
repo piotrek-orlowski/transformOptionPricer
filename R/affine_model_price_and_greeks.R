4c7520c4569fac6fdd1bfdde6ba64d144adae869
Piotr Orłowski <piotrek.orlowski@gmail.com>
2019-09-16 13:08:26 -0400

    the greeks pricer works with values of CF at the transform points

--- R/affine_model_price_and_greeks.R
+++ R/affine_model_price_and_greeks.R
@@ -1,6 +1,6 @@
 #' Option pricing and Greeks in affine models
 #'
-#' @param strikeMat T x K x 1 array
+#' @param strike_mat T x K x 1 array
 #' @param mkt T x 1 data.frame
 #' @param type call, put or otm options (character)
 #' @param N integer num integration point see Fang and Oosterle 2008
@@ -10,6 +10,7 @@
 #' @param N.factors integer, number of factors in SV model
 #' @param v.0 matrix of states in pricing, 1 x N
 #' @param S.0 numeric, stock price at which options are evaluated
+#' @param ode_sol array N x T x (1 + N.factors) of affine CF coefficients
 #' @param ... further arguments to affineCF
 #'
 #' @return
@@ -17,7 +18,7 @@
 #'
 #' @details This works for a single time period!
 #' 
-option_price_with_greeks <- function(strikeMat
+option_price_with_greeks <- function(strike_mat
                                      , mkt
                                      , type = c("call", "put", "otm")
                                      , N = 192L
@@ -27,7 +28,15 @@ option_price_with_greeks <- function(strikeMat
                                      , N.factors = 1L
                                      , v.0
                                      , S.0
+                                     , ode_sol = NULL
                                      , ...){
+  # Mon Sep 16 10:39:53 2019 ------------------------------
+  # pricing problem dimensions
+  T <- length(mkt$t)
+  stopifnot(length(mkt$t) == dim(strike_mat)[1])
+  S <- 9
+  K <- dim(strike_mat)[2]
+  
   # This is for affine models only Tue Jun 04 13:17:59 2019 ------------------------------
   charFun <- affineCF
   
@@ -41,25 +50,25 @@ option_price_with_greeks <- function(strikeMat
   s_fwd <- S.0 + ds
   s_bwd <- S.0 - ds
   
-  # expand strikeMat for the calculation of Greeks Tue Jun 04 13:18:08 2019 ------------------------------
-  # strikeMat is T x K x 1
+  # expand strike_mat for the calculation of Greeks Tue Jun 04 13:18:08 2019 ------------------------------
+  # strike_mat is T x K x 1
   # In order to calculate the Greeks, we will have to expand it to T x K x 9 and adjust strikes in a number of positions
-  # strikeMat[,,1]: pricing at S.0, v.0
-  # strikeMat[,,2]: pricing at S.0, v.0 - dv
-  # strikeMat[,,3]: pricing at S.0, v.0 + dv
-  # strikeMat[,,4]: pricing at S.0 + ds, v.0
-  # strikeMat[,,5]: pricing at S.0 - ds, v.0
-  # strikeMat[,,6]: pricing at S.0 - ds, v.0 - dv
-  # strikeMat[,,7]: pricing at S.0 + ds, v.0 + dv
-  # strikeMat[,,8]: pricing at S.0 - ds, v.0 + dv
-  # strikeMat[,,9]: pricing at S.0 + ds, v.0 - dv
+  # strike_mat[,,1]: pricing at S.0, v.0
+  # strike_mat[,,2]: pricing at S.0, v.0 - dv
+  # strike_mat[,,3]: pricing at S.0, v.0 + dv
+  # strike_mat[,,4]: pricing at S.0 + ds, v.0
+  # strike_mat[,,5]: pricing at S.0 - ds, v.0
+  # strike_mat[,,6]: pricing at S.0 - ds, v.0 - dv
+  # strike_mat[,,7]: pricing at S.0 + ds, v.0 + dv
+  # strike_mat[,,8]: pricing at S.0 - ds, v.0 + dv
+  # strike_mat[,,9]: pricing at S.0 + ds, v.0 - dv
   
   # Initialise array of zeros
   strike_mat_expanded <- array(0, c(dim(strike_mat)[c(1L,2L)], 9L))
   
   # Everything is based on the initial strikes, so fill the array with them.
   # For slices (1,2,3) there will be no change to the strike_mat
-  strike_mat_expanded <- lapply(1:9, function(x) strikeMat)
+  strike_mat_expanded <- lapply(1:9, function(x) strike_mat)
   strike_mat_expanded <- do.call(abind, strike_mat_expanded)
   
   # For slices (4,7,9) the stock price is increased by ds
@@ -72,6 +81,20 @@ option_price_with_greeks <- function(strikeMat
   v.0_expanded <- do.call(rbind, list(v.0, v_bwd, v_fwd, v.0, v.0, v_bwd, v_fwd, v_fwd, v_bwd))
   S.0_expanded <- c(S.0, S.0, S.0, s_fwd, s_bwd, s_bwd, s_fwd, s_bwd, s_fwd)
   
+  # Mon Sep 16 10:30:47 2019 ------------------------------
+  # prepare a pre_calc array for cosTransformPricer
+  if(is.null(ode_sol)){
+    pre_calc <- NULL 
+  } else {
+    pre_calc <- affineCFevalCpp(coeffs = ode_sol
+                                , stateMat = v.0_expanded
+                                , retLog = FALSE)
+    
+    pre_calc <- array(pre_calc, dim = c(N,T,S,K))
+    pre_calc <- aperm(pre_calc, perm = c(1,2,4,3))
+  }
+  
+  
   # calculate all option prices Tue Jun 04 13:34:32 2019 ------------------------------
   price_array <- cosTransformPricer(strikeMat = strike_mat_expanded
                                     , mkt = mkt
@@ -82,6 +105,7 @@ option_price_with_greeks <- function(strikeMat
                                     , charFun = charFun
                                     , params.Q = params
                                     , v.0 = v.0_expanded
+                                    , preCalc = pre_calc
                                     , ...)[[type[1L]]]
   
   # the array contains relative option prices, they have to be multiplied by the appropriate stock prices Tue Jun 04 13:40:14 2019 ------------------------------
